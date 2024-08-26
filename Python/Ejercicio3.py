340e117d41ca8be3eea576eb82250cdb70d45e4a
Nachixxs <ignacionogpa@gmail.com>
2024-08-26 14:35:33 -0300

    Ejercicios de Python Clase 1 Hechos

---
+++ Python/Ejercicio3.py
@@ -0,0 +1,5 @@
+#Ejercicio 3: Crear un rango de 3 a 10 pero con incremento de 2 en 2, en lugar de 1 en 1
+print("Rango con valores de inicio 3, fin 10, incremento 2")
+for i in range(3,11,2):
+    if i > 2:
+        print(i)
