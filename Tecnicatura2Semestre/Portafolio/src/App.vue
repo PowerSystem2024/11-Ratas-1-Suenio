0b377368d6358a95ebd876546e4c546080869909
Christian Bonilla <chrisbonilla.psicologia@gmail.com>
2024-10-07 13:39:51 -0300

    Actualizando cambios Portafolio y Python

--- Tecnicatura2Semestre/Portafolio/src/App.vue
+++ Tecnicatura2Semestre/Portafolio/src/App.vue
@@ -1,17 +1,24 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
-
+import Educacion from './components/EducacionComponente.vue';
+import Experiencia from "./components/ExperienciaComponente.vue";
 </script>
 
 <template>
   <header>
+    <!--Barra de navegacion-->
     <NavBar />
+    <!--Componente Datos Personales-->
     <DatosPersonales />
   </header>
   <main>
-    <section id="educacion"><h2>Educación - Cursos</h2></section>
-    <section id="experiencia"><h2>Experiencia</h2></section>
+    <section id="educacion"><h2>Educación - Cursos</h2>
+    <Educacion />
+    </section>
+    <section id="experiencia"><h2>Experiencia</h2>
+      <Experiencia />
+    </section>
     <section id="proyectos"><h2>Proyectos</h2></section>
     <section id="habilidades"><h2>Habilidades</h2></section>
     <section id="intereses"><h2>Intereses</h2></section>
--- Tecnicatura2Semestre/Portafolio/src/components/DatosPersonales.vue
+++ Tecnicatura2Semestre/Portafolio/src/components/DatosPersonales.vue
@@ -1,15 +1,18 @@
 <script setup>
 
 const title = '11 Ratas un Sueño';
-const descripcion = 'Técnico Universitario en Programación - UTN';
-const presentacion = 'Bienvenidos a nuestro portafolio grupal.';
+const logo = [
+  {id:0, name: 'Logo', src: '/src/assets/img/logo.png', url: 'f'},
+  ];
+const descripcion = 'Portafolio Web Grupal 11 Ratas Un Sueño';
+const presentacion = 'Grupo de ratas entusiastas con la meta de seguir aprendiendo y mejorando. Actualmente cursando la Tecnicatura Superior en Programación (TUP) en la Universidad Tecnológica Nacional (UTN) - FRSR, Argentina.';
 const residencia = 'Argentina';
-const telefono = '242425234';
+const telefono = '123456789';
 const redes = [
-  {id:1, name: 'Linkedin', src: '/src/assets/img/linkedin_icon.svg', url: 'f'},
-  {id:1, name: 'Instagram', src: '/src/assets/img/instagram_icon.svg', url: 'f'},
-  {id:1, name: 'GitHub', src: '/src/assets/img/github_icon.svg', url: 'f'},
-  {id:1, name: 'Curriculum', src: '/src/assets/img/cv_resume_icon.svg', url: 'f'}
+  {id:1, name: 'Linkedin', src: '/src/assets/img/linkedin.png', url: 'f'},
+  {id:1, name: 'Instagram', src: '/src/assets/img/instagram.png', url: 'f'},
+  {id:1, name: 'GitHub', src: '/src/assets/img/github.png', url: 'f'},
+  {id:1, name: 'Curriculum', src: '/src/assets/img/cv.png', url: 'f'}
 ];
 
 </script>
@@ -18,6 +21,9 @@ const redes = [
   <section class="datos-personales">
     <div class="card">
       <h1>{{title}}</h1>
+      <a class="icono-grande">
+        <img class="logo-grupo" :src="logo[0].src" alt="Logo">
+      </a>
       <h2>{{descripcion}}</h2>
       <p>{{presentacion}}</p>
       <ul class="container-lista">
@@ -33,7 +39,7 @@ const redes = [
 
 <style scoped>
   .datos-personales{
-    background-color: rgb(97, 97, 97);
+    background-color: rgba(97, 97, 97,.6);
     border-radius: 5px;
     margin: 1rem;
   }
@@ -41,6 +47,22 @@ const redes = [
     padding: 1rem;
     text-align: center;
   }
+
+  .icono-grande {
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 50%;
+    width: 8rem;
+    display: flex;
+    justify-content: center;
+   align-items: center;
+    margin: .5rem auto;
+  }
+
+  .logo-grupo {
+    width: 100%; /* % del tamaño del logo dentro del círculo */
+    border-radius: 50%;
+  }
+
   .container-lista{
     display: flex;
     justify-content: center;
@@ -48,7 +70,7 @@ const redes = [
     padding: 1rem;
 
     li{
-     background-color: #fff;
+     background-color:  rgba(255, 255, 255, 0.8);
      border-radius: 100%;
      height: 2.5rem;
      width: 2.5rem;
