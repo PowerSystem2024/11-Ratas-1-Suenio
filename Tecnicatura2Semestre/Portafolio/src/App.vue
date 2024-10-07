e257b13b27703a4722e9a21194333169e5126708
Christian Bonilla <chrisbonilla.psicologia@gmail.com>
2024-10-07 16:19:40 -0300

    Terminando de actualizar clase 8 Portafolio

--- Tecnicatura2Semestre/Portafolio/src/App.vue
+++ Tecnicatura2Semestre/Portafolio/src/App.vue
@@ -3,10 +3,7 @@ import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
 import Educacion from './components/EducacionComponente.vue';
 import Experiencia from "./components/ExperienciaComponente.vue";
-<<<<<<< HEAD
 import ProyectosComponente from './components/ProyectosComponente.vue';
-=======
->>>>>>> origin/develop
 </script>
 
 <template>
@@ -17,7 +14,6 @@ import ProyectosComponente from './components/ProyectosComponente.vue';
     <DatosPersonales />
   </header>
   <main>
-<<<<<<< HEAD
     <section id="educacion">
       <h2>Educación - Cursos</h2>
       <Educacion />
@@ -30,15 +26,6 @@ import ProyectosComponente from './components/ProyectosComponente.vue';
       <h2>Proyectos</h2>
       <ProyectosComponente />
     </section>
-=======
-    <section id="educacion"><h2>Educación - Cursos</h2>
-    <Educacion />
-    </section>
-    <section id="experiencia"><h2>Experiencia</h2>
-      <Experiencia />
-    </section>
-    <section id="proyectos"><h2>Proyectos</h2></section>
->>>>>>> origin/develop
     <section id="habilidades"><h2>Habilidades</h2></section>
     <section id="intereses"><h2>Intereses</h2></section>
   </main>
