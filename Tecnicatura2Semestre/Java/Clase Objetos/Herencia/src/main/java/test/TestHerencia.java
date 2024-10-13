36176493e7bdb51c39c74e7f9c74894010f0e004
Daiana Jaldin <dai-jaldin@hotmail.com>
2024-10-13 19:04:31 -0300

    Subiendo clase 9 de Java

---
+++ Tecnicatura2Semestre/Java/Clase Objetos/Herencia/src/main/java/test/TestHerencia.java
@@ -0,0 +1,17 @@
+package test;
+import domain.Empleado;
+import domain.Cliente;
+
+import java.util.Date;
+
+public class TestHerencia {
+  public static void main(String[] args) {
+    Empleado empleado1 = new Empleado("Daai", 24000);
+    System.out.println("empleado1 = " + empleado1);
+    //Si en la clase Empleado no hemos creado nada, nos mostraría valores por defecto.
+    //Esto porque hereda los atributos de Persona
+
+    Cliente cliente1 = new Cliente("Pepito", new Date(), false);
+    System.out.println("cliente1 = " + cliente1);
+  }
+}
