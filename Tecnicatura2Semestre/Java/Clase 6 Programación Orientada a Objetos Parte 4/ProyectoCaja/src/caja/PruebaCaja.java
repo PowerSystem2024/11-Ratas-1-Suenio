e04bd13dbfcf609b4799fd4867b69cd20c9af06b
Nacho0150 <ignacioibanez01@gmail.com>
2024-11-04 02:11:37 -0300

    Revert "Reapply "Merge branch 'develop' into AugustoJVL-patch-1""
    This reverts commit c6bd3133a6e30123642dcf2dfaa76ffc878d7538.

--- Tecnicatura2Semestre/Java/Clase 6 Programación Orientada a Objetos Parte 4/ProyectoCaja/src/caja/PruebaCaja.java
+++
@@ -1,9 +0,0 @@
-package caja;
-
-public class PruebaCaja {
-    public static void main(String[] args) {
-        Caja caja1 = new Caja(5, 5, 5);
-        int volumen = caja1.calcularVolumen();
-        System.out.println("El volumen de la caja es: " + volumen);
-    }
-}
--- Tecnicatura2Semestre/Python/Clase04/EjercicioSumarParesEnRango.py
+++
@@ -1,9 +0,0 @@
-# Ejercicio: Sumar números pares dentro de un rango
-
-sumaPares = 0
-for i in range(1,6):
-  if i % 2 == 0:
-    sumaPares += i
-print(sumaPares)
-
-# También se puede cambiar los parámentros de range por unos que ingrese el usuario
